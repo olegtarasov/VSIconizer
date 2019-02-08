44643b2ac7b080417c71f4f5e8e9e0bcf656ba6e
Oleg Tarasov <oleg.v.tarasov@gmail.com>
2019-02-08 14:59:59 +0300

    VS 2019 support, single version for 2017 and 2019.

--- _VSIconizer/Properties/AssemblyInfo.cs
+++ _VSIconizer/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("VSIconizer.Vs2017")]
+[assembly: AssemblyTitle("VSIconizer")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("VSIconizer.Vs2017")]
+[assembly: AssemblyProduct("VSIconizer")]
 [assembly: AssemblyCopyright("")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
